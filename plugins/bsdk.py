d2172feb9e52cc13a99a25d1ac4382766d8f0de2
Chandra Mohan <97940904+loverobot10@users.noreply.github.com>
2022-02-18 13:31:46 +0530

    Update bsdk.py

--- plugins/bsdk.py
+++ plugins/bsdk.py
@@ -1,4 +1,4 @@
-# Credit and special thanks to @akshat_gosain for figuring out how to fix.
+# plugin
 
 import random
 bsdk = ["Phatele Nirodh Ke Natije!","Chut Ka Maindak…","Abla Naari, Tere Bable Bhaari…","Chut Ke Pasine Mein Talay Hue Bhajiye…","Chullu Bhar Muth Mein Doob Mar!","Kaali Chut Ke Safed Jhaant…","Gote Kitne Bhi Badey Ho, Lund Ke Niche Hi Rehtein Hain…","Naa Chut, Naa Choche, Aur Nakhre Noor Jahan Ke!","Teri Gaand Mein Kutte Ka Lund…","Teri Jhaatein Kaat Kar Tere Mooh Par Laga Kar Unki French Beard Bana Doonga!"]
